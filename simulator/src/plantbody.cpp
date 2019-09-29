549556305ac8e5b1449421d3aef623f90c81df7d
Jacob Morris <jamorris1819@gmail.com>
2019-09-29 21:24:22 +0100

    Added poisson disk sampling and used it create berries on the bushes.

--- simulator/src/plantbody.cpp
+++ simulator/src/plantbody.cpp
@@ -1,5 +1,6 @@
 #include "plantbody.h"
 #include "contactlistener.h"
+#include <poisson_disk_sampling.h>
 #define PI 3.14159265359
 
 PlantBody::PlantBody(GLuint shader, b2World* world) : Body(shader, world)
@@ -94,6 +95,42 @@ void PlantBody::generateRoundBody()
 			// Create new vertex.
 			Vertex nV(x + v.getPosition().x, y + v.getPosition().y);
 			nV.setColour(v.getColour());
+			subVertices.push_back(nV);
+		}
+		p->setVertices(subVertices);
+		p->load();
+		polygons.push_back(p);
+	}
+
+	// Shrink the vertices down to be reused for the additional polygons.
+	float fruitScale = 1.0f + (rand() % 10) / 10.0f;
+	for (int i = 0; i < vertices.size(); i++) {
+		vertices[i].setPosition(vertices[i].getPosition() * 0.1f * fruitScale);
+	}
+
+	namespace pds = thinks::poisson_disk_sampling;
+	auto radius = fruitScale * 0.75f * 0.5f;
+	const auto x_min = std::array<float, 2>{ { -size, -size }};
+	const auto x_max = std::array<float, 2>{ { size, size }};
+
+	// Samples returned as std::vector<std::array<float, 2>>.
+	// Default seed and max attempts.
+	std::vector<std::array<float, 2>> samples = pds::PoissonDiskSampling(radius, x_min, x_max, 30U, rand());
+
+	for (int i = 0; i < samples.size(); i++) {
+		std::array<float, 2> sample = samples[i];
+
+		PolygonR* p = new PolygonR(shaderID);
+		vector<Vertex> subVertices;
+
+		for (int j = 0; j < vertices.size(); j++) {
+			Vertex& v = vertices[j];
+
+
+			// Create new vertex.
+			Vertex nV(0, 0);
+			nV.setPosition(v.getPosition() + glm::vec2(sample[0], sample[1]));
+			nV.setColour(glm::vec3(1, 0, 0));
 			subVertices.push_back(nV);
 		}
 		p->setVertices(subVertices);
